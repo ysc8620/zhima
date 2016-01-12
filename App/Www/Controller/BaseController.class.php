8a9101be07697c7cef59a8eac998b2631cd15f1f
ysc8620 <ysc8620@163.com>
2016-01-12 12:40:23 +0800

    ds

--- App/Www/Controller/BaseController.class.php
+++ App/Www/Controller/BaseController.class.php
@@ -58,6 +58,10 @@ class BaseController extends Controller {
 
         $signPackage = \Wechat\Wxapi::getSignPackage();
         $this->signPackage = $signPackage;
+        $this->share_title = "凑红包, 有福利, 你懂得";
+        $this->share_link = "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+        $this->share_imgUrl = "http://wx.qlogo.cn/mmopen/ajNVdqHZLLDAfibueE3xW0ibPC0MhkhV35kxMBhyv63ZAyHibvQMCm2eJBo5qkltkQiaRgMfLpDE0REA427OwjdxTQ/0";
+        $this->share_desc = "凑红包, 有福利, 你懂得.";
     }
 
     protected function display($templateFile='',$charset='',$contentType='',$content='',$prefix='') {
