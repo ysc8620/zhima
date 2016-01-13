c0d0354dd6cb57b75bc834a64369f5bc08dd263e
ysc8620 <ysc8620@163.com>
2016-01-13 23:20:05 +0800

    ds

--- Inc/Library/Wxpay/jsapi.php
+++ Inc/Library/Wxpay/jsapi.php
@@ -24,14 +24,14 @@ $openId = $tools->GetOpenid();
 
 //②、统一下单
 $input = new WxPayUnifiedOrder();
-$input->SetBody("test");
+$input->SetBody($data['body']);
 $input->SetAttach("test");
 $input->SetOut_trade_no(WxPayConfig::MCHID.date("YmdHis"));
 $input->SetTotal_fee("1");
 $input->SetTime_start(date("YmdHis"));
 $input->SetTime_expire(date("YmdHis", time() + 600));
 $input->SetGoods_tag("test");
-$input->SetNotify_url("http://paysdk.weixin.qq.com/example/notify.php");
+$input->SetNotify_url("http://hb.kakaapp.com/weixin/notify.html");
 $input->SetTrade_type("JSAPI");
 $input->SetOpenid($openId);
 $order = WxPayApi::unifiedOrder($input);
