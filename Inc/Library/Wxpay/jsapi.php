5eb250554be57a3dd1a77de548397687d2bfa89e
ysc8620 <ysc8620@163.com>
2016-01-14 07:40:50 +0800

    ds

--- Inc/Library/Wxpay/jsapi.php
+++ Inc/Library/Wxpay/jsapi.php
@@ -36,8 +36,8 @@ $input->SetTrade_type("JSAPI");
 $input->SetOpenid($openId);
 
 $order = WxPayApi::unifiedOrder($input);
-//echo '<font color="#f00"><b>统一下单支付单信息</b></font><br/>';
-//printf_info($order);
+echo '<font color="#f00"><b>统一下单支付单信息</b></font><br/>';
+printf_info($order);
 $jsApiParameters = $tools->GetJsApiParameters($order);
 
 //获取共享收货地址js函数参数
