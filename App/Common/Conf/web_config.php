564b28da55fa2dbd3604ba07f3b80e0fb9b286bb
ysc8620 <ysc8620@163.com>
2016-01-14 07:32:11 +0800

    ds

--- App/Common/Conf/web_config.php
+++ App/Common/Conf/web_config.php
@@ -1,1 +1,1 @@
-<?php	return array ( 'sitename' => '芝麻乐开源众筹系统', 'domain' => 'http://www.zmlcms.com', 'logo' => '/uploads/1/20151017/zmlcms_1445061661767.png', 'title' => '芝麻乐开源众筹系统', 'keywords' => '芝麻乐众筹管理后台', 'desc' => '芝麻乐众筹管理后台', 'huancun' => '3600', 'upload_exts' => '', 'rootPath' => '', );?>
+<?php	return array ( 'sitename' => '凑红包系统', 'domain' => 'http://hb.kakaapp.com', 'logo' => '/uploads/1/20151017/zmlcms_1445061661767.png', 'title' => '凑红包系统', 'keywords' => '凑红包管理后台', 'desc' => '凑红包管理后台', 'huancun' => '3600', 'upload_exts' => '', 'rootPath' => '', );?>
