54ed8ae162a014260c7e801003f709c5291282a2
ysc8620 <ysc8620@163.com>
2016-02-13 10:29:16 +0800

    ds

--- image.php
+++ image.php
@@ -1,7 +1,11 @@
 <?php
 ini_set('display_errors',1);
-header('Content-type: image/jpeg');
-$image = new Imagick('1.jpg');
-$image->gaussianBlurImage(30,3);
+if(file_exists('1.jpg')){
+    header('Content-type: image/jpeg');
+    $image = new Imagick('1.jpg');
+    $image->gaussianBlurImage(30,3);
 
-echo $image->getImageBlob();
+    echo $image->getImageBlob();
+}else{
+    echo 'file no exit';
+}
