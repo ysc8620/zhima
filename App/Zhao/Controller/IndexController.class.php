5385f104aea9592e1dc5e81b560924fc5165d016
ysc8620 <ysc8620@163.com>
2016-02-13 10:38:31 +0800

    ds

--- App/Zhao/Controller/IndexController.class.php
+++ App/Zhao/Controller/IndexController.class.php
@@ -18,7 +18,8 @@ class IndexController extends BaseController {
     }
 
     public function test(){
-        echo 'ok';
+       $img = new \Think\Image(2);
+        $img->open(ROOT_PATH.'1.jpg')->thumb(800,20000)->save(ROOT_PATH.'2.jpg');
 
     }
 }
