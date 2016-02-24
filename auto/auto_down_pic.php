a0c5d3e77071ef101dbb53e7f60ea282e632ece9
ysc8620 <ysc8620@163.com>
2016-02-24 23:11:20 +0800

    ds

--- auto/auto_down_pic.php
+++ auto/auto_down_pic.php
@@ -25,25 +25,44 @@ do{
 
     if(!$item['pic_url']){
         $pic_url = $item['user_id'].'/'.date("Ymd").'/zp_'.time().rand(111,999).'.jpg';
-        if(!file_exists(dirname($root_path ."/uploads/". $pic_url))){
-            mkdir(dirname($root_path . $pic_url), 0777, true);
-        }
         $data['pic_url'] =  $pic_url;
     }else{
         $pic_url = $item['pic_url'];
     }
     M('zhaopian_pic')->where(array('id'=>$item['id']))->save($data);
 
+    $pic_path = $root_path ."/uploads/". $pic_url;
+    if(!file_exists(dirname($pic_path))){
+        mkdir(dirname($pic_path), 0777, true);
+    }
+
+
     echo $pic_url."\r\n";
-    if(!file_exists($pic_url)){
+    if(!file_exists($pic_path)){
         echo $item['media_id']."\r\n";
         $ds = \Wechat\Wxapi::downloadWeixinFile($item['media_id']);
-        \Wechat\Wxapi::saveWeixinFile($root_path ."/uploads/". $pic_url,$ds['body']);
+        \Wechat\Wxapi::saveWeixinFile($pic_path,$ds['body']);
     }
 
     if($item['is_default']){
-        M('zhaopian')->where(array('id'=>$item['zhaopian_id']))->save(array('pic_url'=>$pic_url));
 
+        M('zhaopian')->where(array('id'=>$item['zhaopian_id']))->save(array('pic_url'=>$pic_url));
+        if(file_exists($pic_path)){
+
+            $img = new \Think\Image(\Think\Image::IMAGE_IMAGICK);
+            $img->open($pic_path);
+            $width = $img->width();
+            $height = $img->height();
+            $x = $y = 0;
+            if($width > $height){
+                $x = floor(($width - $height)/2);
+                $width = $height;
+            }elseif($height> $width){
+                $y = floor(($height - $width)/2);
+                $height = $width;
+            }
+            $img->crop($width, $height,$x,$y, 300, 300)->save($pic_path . '_thumb.jpg','jpg');
+        }
     }
     M('zhaopian_pic')->where(array('id'=>$item['id']))->save(array('state'=>1));
 }while(true);
