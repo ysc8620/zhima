2d2b6f12dc8a2b591a47cc6b053cab7c1a825eaa
ysc8620 <ysc8620@163.com>
2016-02-26 11:17:31 +0800

    ds

--- App/Zhao/Controller/BaseController.class.php
+++ App/Zhao/Controller/BaseController.class.php
@@ -69,7 +69,7 @@ class BaseController extends Controller {
                 session('subscribe', $user['subscribe']);
             }
         }
-
+    /*
         $signPackage = \Wechat\Wxapi::getSignPackage();
         $this->signPackage = $signPackage;
         $this->share_title = "凑红包, 有福利, 你懂得";
@@ -77,6 +77,7 @@ class BaseController extends Controller {
         $this->share_imgUrl = "http://$_SERVER[HTTP_HOST]/images/logo.jpg";
         $this->share_desc = "凑红包, 有福利, 你懂得.";
         $this->subscribe = session('subscribe');
+        */
 
         // if(!$this->user_id)
     }
