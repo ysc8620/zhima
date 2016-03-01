e43a2a652dd509406dd5c0a1b9205c1ea5df30ee
ysc8620 <ysc8620@163.com>
2016-03-01 10:26:51 +0800

    ds

--- App/Common/Conf/weixin.php
+++ App/Common/Conf/weixin.php
@@ -1,3 +1,1 @@
-<?php	return array ( 'weixin_regMsg' => '感谢关注^_^~
-一窝蜂是个互联网创业团队，为您提供最新鲜、最超值的水果！
-快点击菜单里的『一窝蜂』，看看有什么好东西吧。', 'weixin_token' => 'CouHongBaoOs', 'weixin_appid' => 'wx0bfc93135bf3062e', 'weixin_appsecret' => 'b8e588b6eb3530f1d5efad7ea3f1359e', );?>
+<?php	return array ( 'weixin_regMsg' => '感谢关注^_^~', 'weixin_token' => 'CouHongBaoOs', 'weixin_appid' => 'wx506e178e4e9096f7', 'weixin_appsecret' => 'b66a31ee4f49032ff3beb76338c4120b', );?>
