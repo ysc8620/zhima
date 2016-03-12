a378acfa817f2d0ac18a9de2e738467811dabfe4
ysc8620 <ysc8620@163.com>
2016-03-12 13:15:00 +0800

    ds

--- App/Bao/Controller/HongbaoController.class.php
+++ App/Bao/Controller/HongbaoController.class.php
@@ -82,7 +82,7 @@ class HongbaoController extends BaseController {
                 $new['goods_tag'] = "BAO";
                 // $openid = ;//session('openid')?session('openid'):cookie('openid');
                 $new['openid'] = $user['openid'];
-                $json['jsApiParameters'] = jsapipay($new, false);
+                $json['jsApiParameters'] = json_decode(jsapipay($new, false));
                 break;
             }else{
                 $json['msg_code'] = 10002;
