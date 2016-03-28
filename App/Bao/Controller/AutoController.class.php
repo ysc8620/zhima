6331c4ca9782b5569e00fac5f375248169a09c8c
ysc8620 <ysc8620@163.com>
2016-03-28 15:58:57 +0800

    ds

--- App/Bao/Controller/AutoController.class.php
+++ App/Bao/Controller/AutoController.class.php
@@ -28,12 +28,6 @@ class AutoController extends Controller {
         $list = M('bao')->where(array('state'=>2, 'addtime'=>array('lt', time() - 259200), 'is_refund'=>0))->select();
         if($list){
             foreach($list as $hongbao){
-                if(!$hongbao['refund_sn']){
-                    $order_sn = get_order_sn('BR');
-                    M('bao')->where(array('id'=>$hongbao['id']))->save(array('refund_sn'=>$order_sn));
-                }else{
-                    $order_sn = $hongbao['refund_sn'];
-                }
 
                 $total_amount = M('bao_order')->where(array('bao_id'=>$hongbao['id'],'user_id'=>'0','state'=>1))->sum('amount');
 
@@ -48,16 +42,16 @@ class AutoController extends Controller {
 
                 }
                 if($refund_amount > 0){
-                    $rs = refund(array('out_trade_no'=>$order_sn, 'total_fee'=>$hongbao['total_amount']*100, 'refund_fee'=>$refund_amount*100));
+                    $rs = refund(array('out_trade_no'=>$hongbao['order_sn'], 'total_fee'=>$hongbao['total_amount']*100, 'refund_fee'=>$refund_amount*100));
 
                     if($rs['return_code'] == 'SUCCESS' && $rs['result_code'] == 'SUCCESS'){
                         M('bao')->where(array("id"=>$hongbao['id']))->save(array('is_refund'=>1,'state'=>3, 'refund_time'=>time()));
-                        $log = "订单退款成功, 红包编号：{$hongbao['id']},订单编号：{$order_sn}";
+                        $log = "订单退款成功, 红包编号：{$hongbao['id']},订单编号：{$hongbao['order_sn']}";
                         f_log($log, ROOT_PATH.'Runtime/Logs/refund.log');
                         echo $log."<br/>";
                     }else{
                         print_r($rs);
-                        $log = "订单退款失败, 红包编号：{$hongbao['id']},退款订单编号：{$order_sn}";
+                        $log = "订单退款失败, 红包编号：{$hongbao['id']},退款订单编号：{$hongbao['order_sn']}";
                         f_log($log, ROOT_PATH.'Runtime/Logs/refund.log');
                         echo $log."<br/>";
                     }
