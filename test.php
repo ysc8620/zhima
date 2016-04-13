70b762767bd3d5b83911efc82d67548500f1a9a3
ysc8620 <ysc8620@163.com>
2016-04-13 12:58:11 +0800

    ds

--- test.php
+++ test.php
@@ -6,7 +6,10 @@
  * Time: 上午10:54
  * To change this template use File | Settings | File Templates.
  */
+header("Content-type:text/html;charset=utf-8");
+echo "<pre>";
 print_r($_SERVER);
 print_r($_COOKIE);
 print_r($_REQUEST);
+echo "</pre>";
 #exec("./http://sh.kakaapp.com/index.php?s=/bao/hongbao/qrcode.html&id=20160321103653575449&show=yes");
