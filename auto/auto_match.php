796f94b4425088ab577b89481bfb8df15d32ce71
ysc8620 <ysc8620@163.com>
2016-04-20 16:33:14 +0800

    ds

--- auto/auto_match.php
+++ auto/auto_match.php
@@ -15,7 +15,7 @@ class Automatch{
     public function __construct($data){
         global $root_path;
         $this->msg = $data;
-        $this->time = time() - 60000;
+        $this->time = time() -1200;
         $this->qun = M('qun')->where( "UserName='{$data['user']['id']}'")->find();
         $this->user = M('qun_user')->where("UserName='{$data['content']['user']['id']}'")->find();
         $this->json = $json = array(
